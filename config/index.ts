49cc0054edbb9ccbd7e2cd5231952a5cd9901df4
Mykyta Herashchenko <1991224+atre@users.noreply.github.com>
2023-12-25 07:44:05 +0100

    update config

--- config/index.ts
+++ config/index.ts
@@ -3,6 +3,8 @@ import { EnvProps } from 'trn-components/lib/azure/interfaces';
 
 export const env: EnvProps = {
   name: process.env.NAME || 'trn-azure',
+  env: process.env.ENV || 'dev',
   location: process.env.LOCATION || 'westeurope',
   resourceGroupName: process.env.RESOURCEGROUPNAME || 'trn-azure',
+  url: process.env.URL || 'example.com',
 };
